f7164cb0eb47fe2c74c6be1b9b65a139fdd341de
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-10-25 15:09:29 -0500

    add comment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-//import {playAction, nextAction, stopAction} from './actions'
+import {addComment} from './actions'
 import {connect} from 'redux-zero/react'
 
+/*const Post = ({comments}) =>{
+  return(
+    <div key={index}>{guest.name}
+      <br/>{guest.comment}
+    </div>
+  )
+}*/
+
 const App = ({comments}) => {
+  const onSubmit = (e) => {
+		e.preventDefault();
+		console.log ( 'this..', this);//con truco, es el connect el this.
+		addComment(this.playerInputRef.value)
+  }
+  
   const commentsList = comments.map((guest, index)=>{
     return(
     <li key={index}>{guest.name}
       <br/>{guest.comment}
     </li>)
   });
+
   return (
     <div className="wrapper">
       <header>
         <p> New Comment </p>
-        <form >
-          <input type="text" placeholder="User"/>      
-          <input type="text" placeholder="Comment"/>
+        <form onSubmit={onSubmit}>
+          <input type="text" placeholder="User" />      
+          <input type="text" placeholder="Comment" ref={(e) => this.playerInputRef = e}/>
           <button type="submit">Post Comment</button>
         </form>         
       </header>
       <div className="main">	
         <h2>Comments</h2>
-        <p>model.guests.length</p>
+        <p> {comments.length}</p>
         <ul>
           {commentsList}
         </ul>
--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'redux-zero/react'
+import { Provider } from 'redux-zero/react'
 import store from './store'
 
 
 const Index = () => (
-    
-    <Provider store= {store}>
-        <App />
-    </Provider>
+	<Provider store={store}>
+		<App />
+	</Provider>
 )
 
 ReactDOM.render(<Index />, document.getElementById('root'));
